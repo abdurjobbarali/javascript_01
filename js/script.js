f32791e23f96fbecb1899253eeac426452e62992
abdurjobbar <mdabdurjobbarali651268@gmail.com>
2026-01-07 00:15:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// 5 x 1 = 5
+let namtaNumber = 5;
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*1  );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*2  );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*3  );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*4 );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*5 );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*6  );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*7  );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*8  );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*9 );
+console.log(namtaNumber +  " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*10  );
+
+
+
+
